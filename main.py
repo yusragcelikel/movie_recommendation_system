62516876d32f3bc2595694b4ace36f32677b7bb4
yusragcelikel <yusragcelikel@gmail.com>
2023-12-30 20:01:53 +0300

    dataset loaded

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+movie_dataset = pd.read_csv("/Users/yusragokcecelikel/Documents/GitHub/movie_recommendation_system/MoviesOnStreamingPlatforms.csv", index_col=None)
 
