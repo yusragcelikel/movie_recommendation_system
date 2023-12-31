8af5d2c0a00a1b12dd1c7572e01551a05c104295
yusragcelikel <yusragcelikel@gmail.com>
2023-12-31 07:36:04 +0300

    unnamed column removed

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-movie_dataset = pd.read_csv("/Users/yusragokcecelikel/Documents/GitHub/movie_recommendation_system/MoviesOnStreamingPlatforms.csv", index_col=None)
-
+movie_dataset = pd.read_csv("/Users/yusragokcecelikel/Documents/GitHub/movie_recommendation_system/MoviesOnStreamingPlatforms.csv")
+movie_dataset = movie_dataset.iloc[:,1:]
+print(movie_dataset.columns)
