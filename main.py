7e4b09fb9abda9ca514a1c10065db54d9be51167
yusragcelikel <yusragcelikel@gmail.com>
2024-01-07 09:07:29 +0300

    checking unique genre types

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ import seaborn as sns
 movie_dataset = pd.read_csv("/Users/yusragokcecelikel/Documents/GitHub/movie_recommendation_system/imdb_top_1000.csv")
 movie_dataset = movie_dataset.iloc[:,1:] #remove Poster_Link column
 #print(movie_dataset.head())
-print(movie_dataset.info())
+#print(movie_dataset.info())
+
+print(movie_dataset.Genre.unique()) #checking unique genre types
+
+#checking distribution of the genres
+#plt.figure()
