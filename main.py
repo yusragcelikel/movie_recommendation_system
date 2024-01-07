b9428f825e7faa44226f36f6f1fecdd38df1f733
yusragcelikel <yusragcelikel@gmail.com>
2024-01-07 07:32:09 +0300

    dataset update

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-movie_dataset = pd.read_csv("/Users/yusragokcecelikel/Documents/GitHub/movie_recommendation_system/Top_10000_Movies.csv")
-#movie_dataset = movie_dataset.iloc[:,1:] #remove unnamed column
+movie_dataset = pd.read_csv("/Users/yusragokcecelikel/Documents/GitHub/movie_recommendation_system/imdb_top_1000.csv")
+movie_dataset = movie_dataset.iloc[:,1:] #remove unnamed column
+print(movie_dataset.columns)
