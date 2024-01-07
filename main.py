cfe4a91a23b61e8cceb0d3613d880228adde9fd8
yusragcelikel <yusragcelikel@gmail.com>
2024-01-07 08:38:49 +0300

    Understanding the Dataset

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 movie_dataset = pd.read_csv("/Users/yusragokcecelikel/Documents/GitHub/movie_recommendation_system/imdb_top_1000.csv")
-movie_dataset = movie_dataset.iloc[:,1:] #remove unnamed column
-print(movie_dataset.columns)
+movie_dataset = movie_dataset.iloc[:,1:] #remove Poster_Link column
+#print(movie_dataset.head())
+print(movie_dataset.info())
