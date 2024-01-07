9e6361a5a9c914d3c26d7725bbb29447b5253be7
yusragcelikel <yusragcelikel@gmail.com>
2024-01-07 09:46:15 +0300

    handling the multi-level index

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ movie_dataset = movie_dataset.iloc[:,1:] #remove Poster_Link column
 
 #handling multiple genres given in a single cell of dataframe
 genres = movie_dataset['Genre'].str.split(',').apply(pd.Series, 1).stack() #split the values by comma and stack them one after the other
+#print(genres.head(20))
+
+genres.index = genres.index.droplevel(-1) #to drop the last level of the index
 print(genres.head(20))
 
 
